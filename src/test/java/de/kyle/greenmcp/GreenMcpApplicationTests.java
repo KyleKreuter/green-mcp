3503e30927f838668f375f25af20a68fb9b0d99f
Kyle Kreuter <73177493+KyleKreuter@users.noreply.github.com>
2025-12-11 15:23:32 +0100

    Initial Commit

---
+++ src/test/java/de/kyle/greenmcp/GreenMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kyle.greenmcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenMcpApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
