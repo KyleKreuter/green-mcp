465a243d1f38e5ad04613d9bbcecc74031311dd4
Kyle Kreuter <k.kreuter@it-brb.de>
2025-12-11 15:23:32 +0100

    Initial Commit

---
+++ src/main/java/de/kyle/greenmcp/GreenMcpApplication.java
@@ -0,0 +1,13 @@
+package de.kyle.greenmcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenMcpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GreenMcpApplication.class, args);
+  }
+
+}
